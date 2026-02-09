6cffd82515ddd9c1f5aaa920e990c30babf71ef6
Marko <202842510+Dema2101@users.noreply.github.com>
2026-02-09 22:59:50 +0100

    Gsap zoom into window

---
+++ script2.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("load", () => {
+  const originX = "50%";
+  const originY = "45.5%";
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".intro",
+      start: "top top",
+      end: "+=4000",
+      scrub: true,
+      pin: true,
+      anticipatePin: 1,
+      invalidateOnRefresh: true
+      // markers: true
+    }
+  });
+
+  // 1) zoom u prozor
+  tl.to(".scene", {
+    scale: 4.2,
+    transformOrigin: `${originX} ${originY}`,
+    ease: "none"
+  });
+
+  // 2) "enter" - bg nestaje, window preuzima ekran
+  tl.to(".scene-bg", { opacity:0, ease: "none" }, ">");
+
+  tl.to(".window-content", {
+    scale: 4.5,
+    ease: "none"
+  }, "<");
+
+  ScrollTrigger.refresh();
+});
